daf815147995250bede1f1bef03f25bd610a78b4
Magnus Holm <judofyr@gmail.com>
2010-01-25 18:12:37 +0100

    Add some documentation

--- lib/sexp_builder.rb
+++ lib/sexp_builder.rb
@@ -3,10 +3,14 @@ require 'thread'
 require 'sexp_path'
 
 class SexpBuilder
+  VERSION = "0.1"
+  
   autoload :Context,      'sexp_builder/context'
   autoload :QueryBuilder, 'sexp_builder/query_builder'
   attr_reader :context, :scope
   
+  # Initializes the builder. If you redefine this in your subclass,
+  # it's important to call +super()+.
   def initialize
     @context = self.class.current_context
     @main = @context.main_context
@@ -15,6 +19,7 @@ class SexpBuilder
     @query_builders = {}
   end
   
+  # Process +sexp+ under the current context.
   def process(sexp, options = {})
     @scope.unshift(sexp.sexp_type)
 
@@ -36,6 +41,7 @@ class SexpBuilder
     @scope.shift     
   end
   
+  # Process +sexp+ under the top context.
   def process_main(sexp, options = {})
     prev, @context = @context, @main
     process(sexp, options)
@@ -43,25 +49,28 @@ class SexpBuilder
     @context = prev
   end
   
+  # Returns an array in the format of:
+  #
+  #   [<SexpPath::Matcher> rule, <Method> rewriter] 
   def rewriters(context = @context)
-    @rewriters[context] ||= begin
-      context.rewriters.map do |rule|
-        [query_builder(context).send(rule), method("rewrite_#{rule}")]
-      end
+    @rewriters[context] ||= context.rewriters.map do |rule|
+      [query_builder(context).send(rule), method("rewrite_#{rule}")]
     end
   end
   
+  # Returns the query builder for a given context.
   def query_builder(context = @context)
-    @query_builders[context] ||= begin
-      QueryBuilder.make(context, self)
-    end
+    @query_builders[context] ||= QueryBuilder.make(context, self)
   end
   
   class << self
     extend Forwardable
+    # The Context which this builder will process under.
     attr_accessor :current_context
+    # Delegates various methods to the current_context.
     def_delegators :@current_context, :context, :matcher, :rule, :rewrite
     
+    # Sets up the +current_context+.
     def inherited(mod)
       if self == SexpBuilder
         mod.current_context = Context.new(mod)
@@ -70,6 +79,11 @@ class SexpBuilder
       end
     end
     
+    # Turns a module into a context name:
+    #
+    # * FooBar        => foo_bar
+    # * FooBarContext => foo_bar
+    # * FooBarBuilder => foo_bar
     def context_name(mod)
       name = mod.name
       name = name.split("::").last
@@ -77,6 +91,7 @@ class SexpBuilder
       name.scan(/.[^A-Z]+/).map { |part| part.downcase }.join("_")
     end
     
+    # Generates a temporary id.
     def tmpid
       @tmpid ||= 0
       @tmpid += 1
--- lib/sexp_builder/context.rb
+++ lib/sexp_builder/context.rb
@@ -1,7 +1,6 @@
 class SexpBuilder
   class Context
-    attr_reader :parent, :builder, :query_scope,
-                :contexts, :rewriters
+    attr_reader :parent, :builder, :query_scope, :contexts, :rewriters
     
     def initialize(builder, parent = nil)
       @builder = builder
@@ -15,6 +14,7 @@ class SexpBuilder
       end
     end
     
+    # Returns the top-most context.
     def main_context
       if @parent
         @parent.main_context
@@ -23,12 +23,30 @@ class SexpBuilder
       end
     end
     
+    # Defines or finds a sub-context, and evalutes the block under it. If you
+    # want to process something under this context, you would have to call
+    # <tt>process_{name}</tt>.
     def context(name, &blk)
       context = define_context(name.to_sym)
       context.instance_eval(&blk) if blk
       context
     end
     
+    # Defines a matcher. A matcher is a bit of Ruby code which can be used in
+    # your rules. The expression it should match is passed in, and it should
+    # return a true-ish value if it matches. The matcher will be evaluated
+    # under the instatiated processor, so you can use other instance methods
+    # and instance variables too.
+    #
+    #   matcher :five_arguments do |exp|
+    #     self             # => the instance of Example
+    #     exp.length == 6  # the first will always be :arglist
+    #   end
+    # 
+    # Under the hood, this will define:
+    # 
+    # * The block as +matcher_five_arguments+ on the builder.
+    # * +five_arguments+, which will invoke the matcher, on the query scope.
     def matcher(name, &blk)
       method_name = "matcher_#{name}"
       define(method_name, &blk)
@@ -40,6 +58,33 @@ class SexpBuilder
       end
     end
     
+    # Defines a rule. Rules are simply snippets of SexpPath which can refer to
+    # each other and itself. They can also take arguments too.
+    # 
+    #   # Matches any number.
+    #   rule :number do |capture_as|
+    #     # Doesn't make very much sense to take an argument here,
+    #     # it's just an example
+    #     s(:lit, _ % capture_as)
+    #   end
+    #
+    #   # Matches a sequence of plusses: 1 + 2 + 3
+    #   rule :plus_sequence do
+    #     s(:call,               # a method call
+    #        number(:number) |   # the receiver can be a number
+    #        plus_sequence,      # or a sequence   
+    #       :+,
+    #       s(:arglist,
+    #        number(:number) |   # the argument can be a number
+    #        plus_sequence       # or a sequence
+    #   end
+    # 
+    # Under the hood, this will define:
+    # 
+    # * The blocks as +real_number+ and +real_plus_sequence+ on the 
+    #   query_scope.
+    # * +number+ and +plus_sequence+ which wraps the methods above as 
+    #   QueryBuilder::Deferred.
     def rule(name, &blk)
       real_name = "real_#{name}"
       define_query_scope(real_name, &blk)
@@ -48,6 +93,39 @@ class SexpBuilder
       end
     end
     
+    # Defines a rewriter. Rewriters take one or more rules and defines
+    # replacements when they match. The data-object from SexpPath is given as
+    # an argument. If you want some of the sub-expressions matched too, you'll
+    # have to call process() yourself.
+    #
+    #   rewrite :plus_sequence do |data|
+    #     # sum the numbers
+    #     sum = data[:number].inject { |all, one| all + one }
+    #     # return a new number
+    #     s(:lit, sum)
+    #   end
+    # 
+    # Under the hood, this will define:
+    # 
+    # * The block as +rewrite_plus_sequence+.
+    # * And @rewriters will now include :plus_sequence.
+    # 
+    # You can also give this several rules, or none if you want it to match
+    # every single Sexp.
+    #
+    # == Context shortcut
+    #
+    #   rewrite :foo, :in => :bar do
+    #     ...
+    #   end
+    # 
+    # Is the same as:
+    #
+    #   context :bar do
+    #     rewrite :foo do
+    #       ...
+    #     end
+    #   end
     def rewrite(*rules, &blk)
       options = rules.last.is_a?(Hash) ? rules.pop : {}
       rules << :wild if rules.empty?
